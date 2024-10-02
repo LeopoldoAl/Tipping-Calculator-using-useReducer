47e1d8fd42d70d09256c452019d8746b1e910f5b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-02 13:13:50 -0400

    step 16

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import OrderContents from "./components/OrderContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
-import useOrder from "./hooks/useOrder"
 import { initialState, orderReducer } from "./reducers/order-reducer"
 
 function App() {
-
-  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
 
   const [state, dispatch] = useReducer(orderReducer, initialState)
 
@@ -48,7 +45,7 @@ function App() {
                   <OrderTotals 
                     order={state.order}
                     tip={state.tip}
-                    placeOrder={placeOrder}
+                    dispatch={dispatch}
                   />
               </>
             ) : (
