53867bf6d47ee4d7a14095d39a997f3893c41f93
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-02 12:18:12 -0400

    step 8

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,10 @@ function App() {
           </div>
 
           <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-            {order.length ? (
+            {state.order.length ? (
               <>
                   <OrderContents
-                    order={order}
+                    order={state.order}
                     removeItem={removeItem}
                   />
                   <TipPercentageForm 
@@ -46,7 +46,7 @@ function App() {
                     tip={tip}
                   />
                   <OrderTotals 
-                    order={order}
+                    order={state.order}
                     tip={tip}
                     placeOrder={placeOrder}
                   />
